375e7a182375934da92a1c8b7b1a087aa6ff1d65
Nasim <mdnasimhosen333@gmail.com>
2026-02-27 22:59:13 +0600

    cc

--- traffic_inventory_system_frontend/src/redux/api/baseApi.js
+++ traffic_inventory_system_frontend/src/redux/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "http://localhost:5000/api/v1",
-  baseUrl: "https://traffic-inventory-system-1bxi.onrender.com/v1",
+  baseUrl: "https://traffic-inventory-system-1bxi.onrender.com/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
